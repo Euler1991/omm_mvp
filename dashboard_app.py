3fc67142de7c3e671f2cf646ee1c64f7a8ebfb67
Jhonatan Perera <jhonatan.perera@embsoft.com.mx>
2025-07-23 02:12:52 -0600

    initial commit, prueba exitosa

---
+++ dashboard_app.py
@@ -0,0 +1,16 @@
+from dash import Dash, html, dcc, Input, Output, State
+import dash_bootstrap_components as dbc
+
+app = Dash(__name__,external_stylesheets=[dbc.themes.JOURNAL])
+server = app.server
+app.layout = html.Div([html.H1('Olimpiada Mexicana de Matemáticas',
+                               style={'textAlign': 'center'}),
+                       html.H2('Sistema de acceso y descarga de reconocimientos',
+                               style={'textAlign': 'center'}),
+                       dcc.Download(id='download_diploma'),
+                       dcc.Store(id='name_level')
+                       ])
+
+#---------------------------------------------------------------------------------------------------
+if __name__ == '__main__':
+    app.run(debug=False)
